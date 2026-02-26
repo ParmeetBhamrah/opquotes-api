119ef1dcc367e0722f51d7d53b8d794f52521024
Parmeet <parmeetbhamrah@gmail.com>
2026-02-27 00:28:11 +0530

    mount the quote router in app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ const app = express()
 const errorHandler = require('./middleware/errorHandler');
 const characterRoutes = require('./routes/characterRoutes');
 const arcRoutes = require('./routes/arcRoutes');
+const quoteRoutes = require('./routes/quoteRoutes');
 
 app.use(express.json()); // inbuilt middleware for PUT/POST
 
 app.use('/api/characters', characterRoutes);
 app.use('/api/arcs', arcRoutes);
+app.use('/api/quotes', arcRoutes);
 
 app.get('/', (req, res) => {
     res.status(200).json({
